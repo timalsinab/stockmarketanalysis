84d79c5f9304f3f418f4bf1a0c2ff8819f7b7444
timalsinab <timalsinab@berea.edu>
2023-07-03 10:23:46 -0400

    Added comments.

--- main.py
+++ main.py
@@ -26,8 +26,15 @@ def fetch_stock_data():
 
 def create_chart(data):
     if 'Global Quote' in data:
+        "This line extracts the dictionary object with the key 'Global Quote' from the data dictionary. " \
+        "The data dictionary contains the API response data obtained from Alpha Vantage. " \
+        "The 'Global Quote' key holds the specific stock quote information for the requested symbol."
         global_quote = data['Global Quote']
+        "This line retrieves the value associated with the key '05. price' from the global_quote dictionary. " \
+        "The '05. price' key represents the current price of the stock."
         current_price = global_quote['05. price']
+        "This line fetches the value associated with the key '08. previous close' from the global_quote dictionary. " \
+        "The '08. previous close' key corresponds to the previous closing price of the stock."
         previous_close = global_quote['08. previous close']
 
         labels = ['Previous Close', 'Current Price']
