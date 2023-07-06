c91a2a706d8743013147e6807cf63510a285f48d
timalsinab <timalsinab@berea.edu>
2023-07-06 12:06:11 -0400

    Added the data for specific companies and made all of them combine in a single chart.

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pandas as pd
 import yfinance as yf
 import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Function to fetch data and plot all three charts
+# Function to fetch data and plot bar plots for each company
 def fetch_data():
     # Get the current date
     today = datetime.datetime.now().date()
@@ -13,41 +14,58 @@ def fetch_data():
     start_monthly = today - datetime.timedelta(days=365)
     start_yearly = today - datetime.timedelta(days=365 * 10)
 
+    # Define the ticker symbols for the companies
+    tickers = ['MSFT', 'AMZN', 'AAPL', '^GSPC']
+
     # Fetch the data using yfinance
     try:
-        sp500 = yf.download('^GSPC', start=start_weekly, end=today, interval='1wk')
+        data = yf.download(tickers, start=start_weekly, end=today, interval='1wk')
     except Exception as e:
         print('An error occurred:', str(e))
         return
 
-    # Calculate daily returns
-    sp500['daily_return'] = sp500['Close'].pct_change()
+    # Calculate the weekly, monthly, and yearly returns
+    weekly_returns = data['Adj Close'].pct_change().dropna()
+    monthly_returns = data['Adj Close'].resample('M').ffill().pct_change().dropna()
+    yearly_returns = data['Adj Close'].resample('Y').ffill().pct_change().dropna()
 
-    # Drop NaN values
-    sp500.dropna(inplace=True)
+    # Define the x-axis labels for each interval
+    weekly_labels = weekly_returns.index.strftime('%Y-%m-%d')
+    monthly_labels = monthly_returns.index.strftime('%Y-%m')
+    yearly_labels = yearly_returns.index.strftime('%Y')
 
-    # Create subplots for multiple charts
+    # Create subplots for multiple bar plots
     fig, axs = plt.subplots(3, 1, figsize=(10, 12))
 
-    # Plot the weekly chart
-    axs[0].plot(sp500['Close'])
-    axs[0].set_title('Weekly')
+    # Plot the weekly bar plot
+    axs[0].bar(np.arange(len(weekly_returns)), weekly_returns['MSFT'], label='Microsoft')
+    axs[0].bar(np.arange(len(weekly_returns)), weekly_returns['AMZN'], label='Amazon')
+    axs[0].bar(np.arange(len(weekly_returns)), weekly_returns['AAPL'], label='Apple')
+    axs[0].bar(np.arange(len(weekly_returns)), weekly_returns['^GSPC'], label='S&P 500')
+    axs[0].set_xticks(np.arange(len(weekly_returns)))
+    axs[0].set_xticklabels(weekly_labels, rotation=90)
+    axs[0].set_title('Weekly Returns')
+    axs[0].legend()
 
-    # Fetch the data using yfinance for monthly and yearly intervals
-    try:
-        sp500_monthly = yf.download('^GSPC', start=start_monthly, end=today, interval='1mo')
-        sp500_yearly = yf.download('^GSPC', start=start_yearly, end=today, interval='1mo')
-    except Exception as e:
-        print('An error occurred:', str(e))
-        return
+    # Plot the monthly bar plot
+    axs[1].bar(np.arange(len(monthly_returns)), monthly_returns['MSFT'], label='Microsoft')
+    axs[1].bar(np.arange(len(monthly_returns)), monthly_returns['AMZN'], label='Amazon')
+    axs[1].bar(np.arange(len(monthly_returns)), monthly_returns['AAPL'], label='Apple')
+    axs[1].bar(np.arange(len(monthly_returns)), monthly_returns['^GSPC'], label='S&P 500')
+    axs[1].set_xticks(np.arange(len(monthly_returns)))
+    axs[1].set_xticklabels(monthly_labels, rotation=90)
+    axs[1].set_title('Monthly Returns')
+    axs[1].legend()
 
-    # Plot the monthly chart
-    axs[1].plot(sp500_monthly['Close'])
-    axs[1].set_title('Monthly')
-
-    # Plot the yearly chart
-    axs[2].plot(sp500_yearly['Close'])
-    axs[2].set_title('Yearly')
+    # Plot the yearly bar plot
+    axs[2].bar(np.arange(len(yearly_returns)), yearly_returns['MSFT'], label='Microsoft')
+    axs[2].bar(np.arange(len(yearly_returns)),yearly_returns['AMZN'], label='Amazon')
+    axs[2].bar(np.arange(len(yearly_returns)), yearly_returns['AAPL'], label='Apple')
+    axs[2].bar(np.arange(len(yearly_returns)), yearly_returns['^GSPC'], label='S&P 500')
+    axs[2].set_xticks(np.arange(len(yearly_returns)))
+    axs[2].set_xticklabels(yearly_labels, rotation=90)
+    axs[2].set_title('Yearly Returns')
+    axs[2].legend()
 
     # Adjust spacing between subplots
     plt.tight_layout()
@@ -55,5 +73,5 @@ def fetch_data():
     # Show the plot
     plt.show()
 
-# Call the fetch_data() function to fetch data and display the graph
+# Call the fetch_data() function to fetch data and display the bar plots
 fetch_data()
