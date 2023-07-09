802097e20a6bee58810a974ae77c0a574a747f10
timalsinab <timalsinab@berea.edu>
2023-07-09 12:07:22 -0400

    Added the functionality to print the top 5 performing stock from 6 month and last month and also the stock that perform the lowest.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import datetime
 import matplotlib.pyplot as plt
 from matplotlib.widgets import CheckButtons
 
+
 # Function to fetch data and plot line graphs for prices and returns
 def fetch_data():
     # Get the current date
@@ -11,7 +12,7 @@ def fetch_data():
 
     # Calculate the start date for weekly, monthly, and yearly intervals
     start_weekly = today - datetime.timedelta(weeks=52)
-    start_monthly = today - datetime.timedelta(days=365)
+    start_monthly = today - datetime.timedelta(days=30)
     start_yearly = today - datetime.timedelta(days=365 * 10)
 
     # Define the ticker symbols for the companies
@@ -58,7 +59,7 @@ def fetch_data():
     plt.tight_layout()
 
     # Create checkboxes for toggling line visibility
-    checkboxes_ax = plt.axes([0.91, 0.3, 0.08, 0.6])  # Updated position of the checkboxes
+    checkboxes_ax = plt.axes([0.91, 0.35, 0.08, 0.3])
     checkboxes = CheckButtons(checkboxes_ax, tickers, [line_visibility[company] for company in tickers])
 
     # Function to handle checkbox toggling
@@ -76,6 +77,69 @@ def fetch_data():
 
     # Show the plot
     plt.show()
+
+    # Fetch the data for all S&P 500 companies
+    sp500_data = fetch_sp500_data()
+
+    # Check if stock data was fetched successfully
+    if not sp500_data.empty:
+        # Calculate the percentage change for the last 6 months
+        six_month_returns = sp500_data.pct_change(periods=26).iloc[-1]
+
+        # Sort the percentage changes in descending order to get the top performers (last 6 months)
+        top_performers = six_month_returns.sort_values(ascending=False).head(5)
+
+        # Sort the percentage changes in ascending order to get the lowest performers (last 6 months)
+        lowest_performers = six_month_returns.sort_values(ascending=True).head(5)
+
+        # Print the top performing stocks (last 6 months)
+        print("Top Performing Stocks (Last 6 Months):")
+        print(top_performers)
+
+        # Print the lowest performing stocks (last 6 months)
+        print("\nLowest Performing Stocks (Last 6 Months):")
+        print(lowest_performers)
+
+        # Calculate the percentage change for the last month
+        one_month_returns = sp500_data.pct_change(periods=4).iloc[-1]
+
+        # Sort the percentage changes in descending order to get the top performers (last month)
+        top_performers_monthly = one_month_returns.sort_values(ascending=False).head(5)
+
+        # Sort the percentage changes in ascending order to get the lowest performers (last month)
+        lowest_performers_monthly = one_month_returns.sort_values(ascending=True).head(5)
+
+        # Print the top performing stocks (last month)
+        print("\nTop Performing Stocks (Last Month):")
+        print(top_performers_monthly)
+
+        # Print the lowest performing stocks (last month)
+        print("\nLowest Performing Stocks (Last Month):")
+        print(lowest_performers_monthly)
+
+
+# Function to fetch stock data for S&P 500 companies
+def fetch_sp500_data():
+    # Get the list of S&P 500 tickers
+    sp500_tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]['Symbol'].tolist()
+
+    # Create an empty DataFrame to store the stock prices
+    prices_df = pd.DataFrame()
+
+    # Fetch the data for each company using yfinance
+    for ticker in sp500_tickers:
+        try:
+            data = yf.download(ticker, start=datetime.datetime.now() - datetime.timedelta(days=6 * 30),
+                               end=datetime.datetime.now())
+            prices_df[ticker] = data['Adj Close']
+        except Exception as e:
+            print(f'Error fetching data for {ticker}: {str(e)}')
+
+    # Drop any columns with missing values
+    prices_df.dropna(axis=1, inplace=True)
+
+    return prices_df
+
 
 # Call the fetch_data() function to fetch data and display the line graphs with the GUI
 fetch_data()
